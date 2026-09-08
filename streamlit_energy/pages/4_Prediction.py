85ff1890658e4ee4523b8c9b0edbc3036c6de640
Madamedatait <bhlitdataservices@gmail.com>
2026-09-08 12:04:31 +0200

    Change data loading from CSV to Excel format

--- streamlit_energy/pages/4_Prediction.py
+++ streamlit_energy/pages/4_Prediction.py
@@ -37,9 +37,8 @@ charges énergétiques totales.
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-df = pd.read_csv(
-    os.path.join(BASE_DIR, "ENB_data.csv"),
-    sep=";"
+df = pd.read_excel(
+    os.path.join(BASE_DIR, "ENB_data.xlsx")
 )
 
 
